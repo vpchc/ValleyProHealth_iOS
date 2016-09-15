d1706931f7043b302195f214c046004b4fe6b491
Brice Webster <bwebster@vpchc.org>
2016-09-14 21:26:04 -0400

    Back Buttons Setup
    Mostly just setup a view controller for each section and got the back
    buttons running.

---
+++ FormsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FormsViewController.swift
+//  ValleyProHealth
+//
+//  Created by Brice Webster on 9/14/16.
+//  Copyright © 2016 Valley Professionals Community Health Center. All rights reserved.
+//
+
+import UIKit
+
+class FormsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ValleyProHealth/FAQsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  FAQsViewController.swift
+//  ValleyProHealth
+//
+//  Created by Brice Webster on 9/14/16.
+//  Copyright © 2016 Valley Professionals Community Health Center. All rights reserved.
+//
+
+import UIKit
+
+class FAQsViewController: UIViewController {
+
+    @IBOutlet weak var backButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backButtonTap(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
